3ae26e55272bb8dbddbfa04265b1a748ec4cb33a
jay choi <jay.choi@allwon21.com>
2020-07-28 20:41:43 +0900

    component: 코드의 캡슐화, 재사용

---
+++ app.js
@@ -0,0 +1,14 @@
+//component 선언
+Vue.component('num-btn', {
+    template:'<button v-on:click="number++">{{number}}</button>',
+    data: function() {
+        return {
+            number: 0
+        }
+    }
+});
+
+new Vue({
+    el: '#app'
+   
+});
